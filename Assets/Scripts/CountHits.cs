b28e58aa319d24e52e20cabc2c07d230f50ad3c2
Vreme Iulia <ivreme12@gmail.com>
2024-10-24 17:04:25 +0300

    add

---
+++ Assets/Scripts/CountHits.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountHits : MonoBehaviour
+{
+    private int hits = 0;
+    public ScoreManager scoreManager; // Reference to the ScoreManager
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensure the ScoreManager is linked
+        if (scoreManager == null)
+        {
+            scoreManager = FindObjectOfType<ScoreManager>();
+            if (scoreManager == null)
+            {
+                Debug.LogError("ScoreManager not found in the scene. Please ensure it is added.");
+            }
+
+        }
+    }
+
+     
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "fairways")
+        {
+         
+            hits++;
+
+            scoreManager.UpdateScore();
+        }
+    }
+}
